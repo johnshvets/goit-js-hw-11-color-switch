8db363ebca0c75d64e75ec6053e2fa62234ad90c
Ivan Shvets <johnshvets@gmail.com>
2020-10-23 22:56:08 +0300

    Create color-switcher.js

---
+++ js/color-switcher.js
@@ -0,0 +1,44 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  toggleDisabledBtn(true);
+  timerId = setInterval(() => changeBackgroundColor(colors), 1000);
+}
+
+function onStopBtnClick() {
+  toggleDisabledBtn(false);
+  clearInterval(timerId);
+}
+
+function toggleDisabledBtn(value) {
+  refs.startBtn.disabled = value;
+}
+
+function randomIntegerFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function changeBackgroundColor(colors) {
+  const color = randomIntegerFromInterval(0, colors.length);
+
+  refs.body.style.backgroundColor = colors[color];
+}
